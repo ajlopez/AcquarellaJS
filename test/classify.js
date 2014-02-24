92af29098ee5b94736e68d1b7683679c34f55384
ajlopez <webmaster@ajlopez.com>
2014-02-24 06:53:06 -0300

    Whitespaces

--- test/classify.js
+++ test/classify.js
@@ -3,6 +3,17 @@ var classifier = require('../lib/classifier.js');
 
 var Letter = classifier.Letter;
 var Digit = classifier.Digit;
+var Space = classifier.Space;
+
+function all(items, value) {
+    var l = items.length;
+    
+    for (var k = 0; k < l; k++)
+        if (items[k] !== value)
+            return false;
+            
+    return true;
+}
 
 exports['empty string'] = function (test) {
     var result = classifier.classify('');
@@ -29,3 +40,13 @@ exports['digit'] = function (test) {
     test.equal(result.length, 1);
     test.equal(result[0], Digit);
 };
+
+exports['whitespaces'] = function (test) {
+    var result = classifier.classify(' \r\n\t');
+    
+    test.ok(result);
+    test.ok(Array.isArray(result));
+    test.equal(result.length, 4);
+    test.ok(all(result, Space));
+};
+
