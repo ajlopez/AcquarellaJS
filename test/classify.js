bd12df5f5c537e0a35d5273e60c2da78ed834843
ajlopez <webmaster@ajlopez.com>
2014-02-24 06:46:09 -0300

    classify letter, digit

--- test/classify.js
+++ test/classify.js
@@ -1,11 +1,31 @@
 
-var classiffier = require('../lib/classifier.js');
+var classifier = require('../lib/classifier.js');
+
+var Letter = classifier.Letter;
+var Digit = classifier.Digit;
 
 exports['empty string'] = function (test) {
-    var result = classiffier.classify('');
+    var result = classifier.classify('');
     
     test.ok(result);
     test.ok(Array.isArray(result));
     test.equal(result.length, 0);
 };
 
+exports['letter'] = function (test) {
+    var result = classifier.classify('a');
+    
+    test.ok(result);
+    test.ok(Array.isArray(result));
+    test.equal(result.length, 1);
+    test.equal(result[0], Letter);
+};
+
+exports['digit'] = function (test) {
+    var result = classifier.classify('0');
+    
+    test.ok(result);
+    test.ok(Array.isArray(result));
+    test.equal(result.length, 1);
+    test.equal(result[0], Digit);
+};
